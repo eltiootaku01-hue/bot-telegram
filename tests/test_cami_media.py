87ab9c3e8a53963cdf6a9c2d43b8242268fe274f
eltiootaku01-hue <eltiootaku01@gmail.com>
2026-09-10 13:03:04 -0300

    test(cami): cover request association controls

--- tests/test_cami_media.py
+++ tests/test_cami_media.py
@@ -1,4 +1,4 @@
-from app.ui.media_keyboards import cami_media_actions, cami_publish_destination
+from app.ui.media_keyboards import cami_media_actions, cami_pending_requests, cami_publish_destination
 
 
 def test_cami_media_actions_expose_only_private_workflow_actions() -> None:
@@ -6,6 +6,7 @@ def test_cami_media_actions_expose_only_private_workflow_actions() -> None:
     data = [button.callback_data for row in markup.inline_keyboard for button in row]
     assert "cami:media:tag:7" in data
     assert "cami:media:schedule:7" in data
+    assert "cami:media:request:7" in data
     assert "cami:media:archive:7" in data
 
 
@@ -14,3 +15,9 @@ def test_cami_destination_keeps_page_and_group_linked() -> None:
     data = [button.callback_data for row in markup.inline_keyboard for button in row]
     assert "cami:media:dest:both:7" in data
     assert "cami:media:dest:group:7" in data
+
+
+def test_cami_pending_requests_preserve_visible_order_number() -> None:
+    markup = cami_pending_requests(7, [(123, "Asuna con traje de conejita")])
+    data = [button.callback_data for row in markup.inline_keyboard for button in row]
+    assert "cami:req:link:7:123" in data
