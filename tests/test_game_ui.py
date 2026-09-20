39f173d31a75633b9df189dd990cad20c2646c80
eltiootaku01-hue <eltiootaku01@gmail.com>
2026-09-20 17:07:08 -0300

    test: preserve core Sunna game hub actions

--- tests/test_game_ui.py
+++ tests/test_game_ui.py
@@ -11,3 +11,21 @@ def test_game_hub_exposes_daily_missions_action() -> None:
     ]
 
     assert "game:missions:open" in data
+
+
+def test_game_hub_keeps_core_sunna_actions() -> None:
+    markup = game_hub_keyboard()
+    data = {
+        button.callback_data
+        for row in markup.inline_keyboard
+        for button in row
+        if button.callback_data
+    }
+
+    assert {
+        "game:gacha:open",
+        "game:inventory:open",
+        "game:combat:open",
+        "game:trivia:start",
+        "game:mystery:open",
+    } <= data
