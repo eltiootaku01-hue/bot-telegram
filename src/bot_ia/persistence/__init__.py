5ee7842e1d2916061c842725ae6ba422ea28035f
eltiootaku01-hue <eltiootaku01@gmail.com>
2026-09-25 13:15:06 -0300

    feat(persistence): expose shared economy database

---
+++ src/bot_ia/persistence/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Persistencia compartida de BOT-IA."""
+
+from .economy import EconomyDatabase, EconomyPersistenceError
+
+__all__ = ["EconomyDatabase", "EconomyPersistenceError"]
